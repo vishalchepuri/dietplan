144443f50cce0351bf9e6eb5a5daa1da81838aec
vishalchepuri <vishalchepuri1@gmail.com>
2024-04-21 14:10:33 +0530

    added frontend

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
-from flask import Flask, jsonify
-import google.generativeai as genai # Your generative AI import
-import os   
+# Import necessary modules
+from flask import Flask, render_template, request
+import google.generativeai as genai
+import os
 
-
+# Create Flask app instance
 app = Flask(__name__)
 
+# Define function to get Gemini response
 def get_gemini_response(question):
     key = os.environ.get('API_KEY')
     genai.configure(api_key=key)
@@ -12,26 +14,33 @@ def get_gemini_response(question):
     response = model.generate_content(question)
     return response.text
 
-
+# Define route to render index.html
 @app.route('/')
-def all():
-    return "Working"
+def index():
+    return render_template('index.html')
 
-
-@app.route('/dietplan/<height>/<weight>')
-def get_diet_plan(height, weight):
+# Define route to handle form submission and generate diet plan
+@app.route('/dietplan', methods=['GET'])
+# Define route to handle form submission and generate diet plan
+@app.route('/dietplan', methods=['GET'])
+def diet_plan():
+    height = request.args.get('height')
+    weight = request.args.get('weight')
+    gender =  request.args.get('gender')
     prompt = f"""
-    I'm {height} feet tall and weigh {weight} kilograms. Please calculate my Body Mass Index (BMI). Based on the results, I'd like a tailored diet plan to address my needs. Here's what I'm looking for:
+    I'm {height} feet tall and weigh {weight} kilograms and Gender {gender}. Please calculate my Body Mass Index (BMI). Based on the results, I'd like a tailored diet plan to address my needs. Here's what I'm looking for:
     Goal: If my BMI is healthy, provide a balanced diet. If my BMI indicates I'm underweight, provide a weight gain diet. If I'm overweight, provide a weight loss diet.
     Nutritional Targets: Please specify the total protein, calories, and fiber in the diet plan.
     Indian Cuisine: I prefer Indian food options for both a vegetarian and non-vegetarian plan.
     Schedule: Outline a sample daily meal plan with suggestions for breakfast, lunch, and dinner.
     Drinkng water: no. of liters
+    note: In the output nothing should be bold
     """
     response = get_gemini_response(prompt)
-    return jsonify({'diet_plan': response})
+    
+    return render_template('result.html', diet_plan=response)
 
+
+# Run the Flask app
 if __name__ == '__main__':
-    print("I called")
     app.run(host='0.0.0.0', port=5000)
-
